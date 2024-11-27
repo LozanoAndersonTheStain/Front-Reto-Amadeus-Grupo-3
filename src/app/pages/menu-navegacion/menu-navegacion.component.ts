50cf7fcf710bec2683c38180a1bbd44c88ff8c00
Anderson Lozano <adslozanov@gmail.com>
2024-11-27 16:27:55 -0500

    :hammer: Added method to perform sessionStorage cleanup

--- src/app/pages/menu-navegacion/menu-navegacion.component.ts
+++ src/app/pages/menu-navegacion/menu-navegacion.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterLink} from '@angular/router';
-import { IndexComponent } from "../index/index.component";
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { UsuarioComponent } from '../usuario/usuario.component';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-menu-navegacion',
   standalone: true,
-  imports: [RouterLink, IndexComponent, UsuarioComponent],
+  imports: [CommonModule, RouterLink, UsuarioComponent],
   templateUrl: './menu-navegacion.component.html',
   styleUrl: './menu-navegacion.component.css'
 })
-export class MenuNavegacionComponent {
+export class MenuNavegacionComponent implements OnInit {
+  isAdmin = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    if (typeof sessionStorage !== 'undefined') {
+      const userRole = sessionStorage.getItem('name');
+      this.isAdmin = userRole === 'admin';
+    } else {
+      console.log('sessionStorage is not available');
+    }
+  }
+
+  resetSessionStorage(): void {
+    sessionStorage.clear();
+  }
 }
